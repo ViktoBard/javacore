da1519595f20db2721cef986404e39f1039d4d87
ViktoBard <7267197@gmail.com>
2017-05-30 18:04:13 +0300

    GameDotCom

---
+++ src/main/java/com/kitcenter/app/homework/Game/GameHelper.java
@@ -0,0 +1,20 @@
+package com.kitcenter.app.homework.Game;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by user on 30.05.2017.
+ */
+public class GameHelper {
+    public String getUserInput(String promt) {
+        String inputLine = null;
+        System.out.println(promt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            if (inputLine.length()==0) {
+                return null;
+            }
+        }
+    }
+}
